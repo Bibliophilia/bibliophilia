a89ff1573af09ce71df2f072a86f69ea71d561c8
benmasud <rofikulbenmasud@gmail.com>
2024-06-14 22:10:27 +0700

    In progress , need to fix the

--- frontend/src/component/Component-APIs/googleAuthAPI.js
+++ frontend/src/component/Component-APIs/googleAuthAPI.js
@@ -9,27 +9,6 @@ const registerWithGoogle = async (googleResponse) => {
         throw error;
     }
 };
-{/*
-const getUserProfile = async () => {
-    try {
-        const response = await fetch('http://localhost:8000/users/session/get-user', {
-            method: 'GET',
-            headers: {
-                'accept': 'application/json',
-            },
-        });
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch user profile');
-        }
-
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching user profile:', error.message);
-        throw error;
-    }
-};
- */}
 
 export { registerWithGoogle};
