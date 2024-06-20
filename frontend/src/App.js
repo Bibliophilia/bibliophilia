1fc7c332b1ecc27ff1ce0648efd480dcca31139c
benmasud <rofikulbenmasud@gmail.com>
2024-06-20 22:28:00 +0700

    small update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,28 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './Style/App.css';
 import HomePage from './component/Page-Component/HomePage';
 import CategoryWindow from './component/Page-Component/CategoryWindow';
 import SearchResultsPage from './component/Page-Component/SearchResultsPage';
+import BookInfoPage from "./component/Books-Component/BookInfoPage";
 import UploadBooks from "./component/Books-Component/UploadBooks";
 import UserProfile from "./component/User-Component/UserProfile";
-import BookInfoPage from "./component/Books-Component/BookInfoPage";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<UserProfile />} />   // testing if user profile is working or not
-          <Route path="/category" element={<CategoryWindow />} />
-          <Route path="/upload-books" element={<UploadBooks />} />
-          <Route path="/search-results" element={<SearchResultsPage />} />
-          <Route path="/books/:idx" element={<BookInfoPage />} />
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/category" element={<CategoryWindow />} />
+            <Route path="/upload-books" element={<UploadBooks />} />
+            <Route path="/search-results" element={<SearchResultsPage />} />
+            <Route path="/books/:idx" element={<BookInfoPage />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
--- frontend/src/component/Page-Component/HomePage.js
+++ frontend/src/component/Page-Component/HomePage.js
@@ -93,4 +93,7 @@ const HomePage = () => {
     );
 };
 
+
+
+
 export default HomePage;
